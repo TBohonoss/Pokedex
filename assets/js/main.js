e72be8c227492c4b72a901b1a1459dfa371de545
Thiago <thiago.bohonos@gmail.com>
2022-12-05 19:54:30 -0300

    Transformando a lista de pokemons em lista de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,40 @@
-console.log('Sucesso!')
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="green">
+            <span class="number">#001</span>
+            <class class="name">${pokemon.name}</class>
+
+            <div class="detail">
+            <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+
+                <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        }
+
+    })
+    .catch((error) => console.error(error))
+
+
+
+
